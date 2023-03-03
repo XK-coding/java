fa3e4e36fdf9ebdd1495f1a80cc6569733d8f098
XK-coding <110663524+XK-coding@users.noreply.github.com>
2023-03-03 22:24:19 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String args[]) {
-		System.out.println("´ó¼ÒºÃ!");
+		System.out.println("å¤§å®¶å¥½!");
 		System.out.println("Nice to meet you");
 		Student stu = new Student();
 		stu.speak("We are students");
